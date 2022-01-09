9befb207c6fee41655f01c4cc38fa98e8134dc72
Algimantas <algimantasmedeksa@gmail.com>
2022-01-09 20:20:07 +0200

    pr1

---
+++ SeptintasProjektas/SeptintasProjektas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptintasProjektas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite pirmą skaičių");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Įveskite antrą skaičių");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Įveskite trečią skaičių");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a > b && a < 100)
+            {
+                Console.WriteLine("Pirmas skaičius yra didesnis už antrą ir mažesnis už 100.");
+            }
+            if (b > 0 && b > a)
+            {
+                Console.WriteLine("Antras skaičius yra teigiamas ir didesnis už pirmą.");
+            }
+            if ((a > b && a > c) || a > 0)
+            {
+                Console.WriteLine("Pirmas skaičius yra teigiamas, arba didesnis už antrą ir trečią skaičių.");
+            }
+            else if ((c >= 5 && c <= 10) || c>a || c>b)
+            {
+                Console.WriteLine("Trečias skaičius yra lygus arba didesnis už 5 bet mažesnis arba lygus už 10, arba didesnis už pirmą arba antrą skaičių.");
+            }
+            else
+            {
+                Console.WriteLine("Skaičių reikšmės neatitinka nei vienai sąlygai.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
